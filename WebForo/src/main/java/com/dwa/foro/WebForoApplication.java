573f494cbfa59a9d27d4132422208dd6adf71960
JoseCazares <josericardo1928@hotmail.com>
2021-05-31 15:46:19 -0500

    hbj

--- WebForo/src/main/java/com/dwa/foro/WebForoApplication.java
+++ WebForo/src/main/java/com/dwa/foro/WebForoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan ({"com.dwa.foro.controladores, com.dwa.foro.modelo, com.dwa.foro.servicios", "controller", "service"})
+
 public class WebForoApplication {
 
 	public static void main(String[] args) {
